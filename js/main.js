046fdb10c3f5a1e2cd227b9c98c1687fe4a53ee4
qikaromonshim <qikaromonshim@gmail.com>
2026-02-07 16:46:21 +0300

    Simplify click event handling for crystal

--- js/main.js
+++ js/main.js
@@ -5,13 +5,9 @@ const crystal = new Crystal();
 let score = 0;
 
 canvas.addEventListener('click', e => {
-  const rect = canvas.getBoundingClientRect();
-  const mx = e.clientX - rect.left;
-  const my = e.clientY - rect.top;
-
-  if (crystal.isClicked(mx, my)) {
+  if (crystal.isClicked(e.clientX, e.clientY)) {
     score++;
-    crystal.reset();
+    crystal.click();
   }
 });
 
@@ -29,4 +25,3 @@ function animate() {
 }
 
 animate();
-
