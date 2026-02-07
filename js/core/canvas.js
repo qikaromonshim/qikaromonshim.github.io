554a441ea58dde97fd74da282be6bb8f5def83c2
qikaromonshim <qikaromonshim@gmail.com>
2026-02-07 15:17:02 +0300

    Update canvas.js

--- js/core/canvas.js
+++ js/core/canvas.js
@@ -1,1 +1,25 @@
+const canvas = document.getElementById('bgCanvas');
+const ctx = canvas.getContext('2d');
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+window.addEventListener('resize', () => {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  canvas.width = width;
+  canvas.height = height;
+});
+
+const mouse = { x: 0, y: 0 };
+
+window.addEventListener('mousemove', e => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+});
+
+export { canvas, ctx, width, height, mouse };
 
