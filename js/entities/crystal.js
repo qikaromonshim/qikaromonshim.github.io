a8bd8ffb09437c5ca28e520e0230bb2664b66fb8
qikaromonshim <qikaromonshim@gmail.com>
2026-02-07 16:38:49 +0300

    Update crystal.js

--- js/entities/crystal.js
+++ js/entities/crystal.js
@@ -3,6 +3,8 @@ import { ctx, width, height, mouse } from '../core/canvas.js';
 export class Crystal {
   constructor() {
     this.size = 30;
+    this.state = "normal"; 
+    this.explodeTimer = 0;
     this.reset();
   }
 
@@ -27,14 +29,29 @@ update() {
     this.vy += Math.sin(a) * 0.8;
   }
 
-  // hız limiti (çok önemli)
+ 
   this.vx = Math.max(-1.5, Math.min(1.5, this.vx));
   this.vy = Math.max(-1.5, Math.min(1.5, this.vy));
 
-
   if (this.x < 0 || this.x > width) this.vx *= -1;
   if (this.y < 0 || this.y > height) this.vy *= -1;
+
+  if (this.state === "explode") {
+  this.vx = 0;
+  this.vy = 0;
 }
+  if (this.state === "explode") {
+    this.size += 2;
+    this.explodeTimer--;
+
+    if (this.explodeTimer <= 0) {
+      this.reset();
+      this.size = 30;
+      this.state = "normal";
+    }
+  }
+}
+
 
 
   draw() {
@@ -42,10 +59,20 @@ update() {
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fillStyle = 'white';
     ctx.fill();
+    ctx.fillStyle = this.state === "explode" ? "orange" : "white";
+
   }
 
   isClicked(mx, my) {
-    return Math.hypot(this.x - mx, this.y - my) < this.size + 10;
+  const hit = Math.hypot(this.x - mx, this.y - my) < this.size + 10;
+
+  if (hit && this.state === "normal") {
+    this.state = "explode";
+    this.explodeTimer = 10;
+    return true;
   }
+
+  return false;
+ }
 }
 
